3ce63c2d8de0f7dcdaec6668a956630746be55e7
Dreamlike <bersenev@mail.ru>
2023-11-26 13:59:58 +0300

    First commit

--- google_should_find_selene.py
+++ google_should_find_selene.py
@@ -1,7 +1,16 @@
-from selene.support.shared import browser
-from selene import be, have
+# from selene.support.shared import browser
+# from selene import be, have
+import pytest
 
 
-browser.open('https://google.com')
-browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+# browser.open('https://google.com')
+# browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+# browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+@pytest.fixture(scope='session', autouse=True)
+def before_all(request):
+    print("Called before all tests: " + request.node.name)
+
+
+def test_first(before_all):
+    assert 1 == 1
